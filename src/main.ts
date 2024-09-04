a4bac83eaeda74dfaa812382dc85e4140954062e
Palchik <dima21112000@yandex.ru>
2024-09-04 20:18:20 +0300

    delete console log

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-console.log('dsadadsa');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
