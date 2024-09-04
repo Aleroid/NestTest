c4e57da2eb651a31e83b8d4b4b2f7e79c72566f4
Palchik <dima21112000@yandex.ru>
2024-09-04 20:21:45 +0300

    add console log fura

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log("fura")
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
